b9d44703d5fc8261a002c5a028e15e964e675ade
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2025-07-29 14:38:57 +0530

    name

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -77,7 +77,7 @@ export default function Navbar() {
                     isScrolled ? "text-white" : "text-white"
                   }`}
                 >
-                  EchoBlogs
+                  EchoChats
                 </span>
               </Link>
             </div>
